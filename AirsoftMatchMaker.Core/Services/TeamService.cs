ef6c48e21e34d68082a27224651cc48540ce4234
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-02 17:36:56 +0200

    Removed join team methods and moved them to the background team service

--- AirsoftMatchMaker.Core/Services/TeamService.cs
+++ AirsoftMatchMaker.Core/Services/TeamService.cs
@@ -16,6 +16,14 @@ namespace AirsoftMatchMaker.Core.Services
             this.repository = repository;
         }
 
+        public async Task<bool> DoesPlayerHaveTeam(string userId)
+        {
+            var player = await repository.AllReadOnly<Player>()
+                .Where(p => p.UserId == userId)
+                .FirstOrDefaultAsync();
+            return player.TeamId != null;
+        }
+
         public async Task<IEnumerable<TeamListModel>> GetAllTeamsAsync()
         {
             var teams = await repository.AllReadOnly<Team>()
@@ -32,6 +40,7 @@ namespace AirsoftMatchMaker.Core.Services
                 .ToListAsync();
             return teams;
         }
+
 
         public async Task<TeamViewModel> GetTeamByIdAsync(int id)
         {
@@ -71,19 +80,48 @@ namespace AirsoftMatchMaker.Core.Services
             return team;
         }
 
-        public async Task JoinTeamAsync(string userId, int teamId)
+        public async Task<TeamViewModel> GetPlayersTeamAsync(string userId)
         {
             var player = await repository.AllReadOnly<Player>()
-                .Where(p => p.UserId == userId && p.IsActive)
+                .Where(p => p.UserId == userId)
                 .FirstOrDefaultAsync();
 
-            var team = await repository.All<Team>()
-                .Where(t => t.Id == teamId)
+            var team = await repository.AllReadOnly<Team>()
+                .Where(t => t.Players.Contains(player))
                 .Include(t => t.Players)
+                .ThenInclude(t => t.PlayerClass)
+                .Include(t => t.GamesAsTeamRed)
+                .Include(t => t.GamesAsTeamBlue)
+                .Select(t => new TeamViewModel()
+                {
+                    Id = t.Id,
+                    Name = t.Name,
+                    Wins = t.Wins,
+                    Losses = t.Losses,
+                    AverageSkillPoints = t.Players.Count != 0 ? (int)(t.Players.Where(p => p.IsActive).Average(p => p.SkillPoints)) : 0,
+                    Players = t.Players.Where((p) => p.IsActive)
+                    .Select(p => new PlayerMinModel()
+                    {
+                        Id = p.Id,
+                        UserName = p.User.UserName,
+                        SkillLevel = p.SkillLevel,
+                        PlayerClassName = p.PlayerClass.Name
+                    })
+                    .ToList(),
+                    Games = t.GamesAsTeamRed.Union(t.GamesAsTeamBlue).Select(g => new GameMinModel()
+                    {
+                        Id = g.Id,
+                        Name = g.Name,
+                        GameStatus = g.GameStatus,
+                        Date = g.Date.ToShortDateString(),
+                        Result = g.Result != null ? g.Result : "0:0"
+                    })
+                    .ToHashSet()
+                })
                 .FirstOrDefaultAsync();
-
-            team.Players.Add(player);
-            await repository.SaveChangesAsync();
+            return team;
         }
+
+
     }
 }
