c3a6117609424fc16b5f0cc9106b1fcad916616e
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:35:01 +0200

    Added inventory controller. Index method gets all of the players current items and his ammo count.

---
+++ AirsoftMatchMaker.Web/Areas/Player/Controllers/InventoryController.cs
@@ -0,0 +1,29 @@
+﻿using AirsoftMatchMaker.Core.Contracts;
+using AirsoftMatchMaker.Core.Models.Inventory;
+using AirsoftMatchMaker.Web.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirsoftMatchMaker.Web.Areas.Player.Controllers
+{
+    [Area("Player")]
+    [Authorize(Roles = "Player")]
+    public class InventoryController : Controller
+    {
+
+        private readonly IInventoryService inventoryService;
+        public InventoryController(IInventoryService inventoryService)
+        {
+            this.inventoryService = inventoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await inventoryService.GetPlayerItemsAsync(User.Id());
+
+            return View(model);
+        }
+
+
+    }
+}
