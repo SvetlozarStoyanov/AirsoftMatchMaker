75f968002729a52fc95d7be8854e913217f63719
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:49:01 +0200

    Added properties for min and max FeetPerSecond,FireRate,AverageAmmoExpended properties which will be set by weaponService when building the model.

--- AirsoftMatchMaker.Core/Models/Weapons/WeaponCreateModel.cs
+++ AirsoftMatchMaker.Core/Models/Weapons/WeaponCreateModel.cs
@@ -15,14 +15,24 @@ namespace AirsoftMatchMaker.Core.Models.Weapons
         [Required]
         [MinLength(3), MaxLength(200)]
         public string Description { get; set; } = null!;
-        public  double FeetPerSecond { get; init; }
-        public  double FireRate { get; init; }
+        [Range(1, 100, ErrorMessage = "Feet per second must be within given range!")]
+        public double FeetPerSecond { get; set; }
+        [Range(1, 100, ErrorMessage = "Fire rate must be within given range!")]
+        public double FireRate { get; set; }
         public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
-        public int AverageAmmoExpendedPerGame { get; init; }
+        [Range(1, 100, ErrorMessage = "Average ammo expended must be within given range!")]
+        public int AverageAmmoExpendedPerGame { get; set; }
         public WeaponType WeaponType { get; init; }
         public PreferedEngagementDistance PreferedEngagementDistance { get; set; }
         public ICollection<PreferedEngagementDistance> PreferedEngagementDistances { get; set; }
         public decimal FinalImportPrice { get; set; }
+
+        public double MinFeetPerSecond { get; set; }
+        public double MaxFeetPerSecond { get; set; }
+        public double MinFireRate { get; set; }
+        public double MaxFireRate { get; set; }
+        public double MinAverageAmmoExpended { get; set; }
+        public double MaxAverageAmmoExpended { get; set; }
     }
 }
