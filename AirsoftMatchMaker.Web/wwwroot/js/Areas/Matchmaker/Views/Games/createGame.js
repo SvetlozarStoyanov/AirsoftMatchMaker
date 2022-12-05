3e3081f6fe0717486cd262148c9e8043d96b2d73
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:25:00 +0200

    removed unnecessary js file

--- AirsoftMatchMaker.Web/wwwroot/js/Areas/Matchmaker/Views/Games/createGame.js
+++
@@ -1,7 +0,0 @@
-﻿const dateLabel = document.getElementById('dateLabel');
-const inputForModel = document.getElementById('inputForModel');
-
-window.addEventListener('onload', function (e) {
-    inputForModel.value = dateLabel.textContent;
-    console.log(inputForModel.value);
-});
