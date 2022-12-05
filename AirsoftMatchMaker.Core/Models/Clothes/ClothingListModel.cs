df17df5319715476f2697cb7dfb3ae8e814936e6
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:31:41 +0200

    Added Image url

--- AirsoftMatchMaker.Core/Models/Clothes/ClothingListModel.cs
+++ AirsoftMatchMaker.Core/Models/Clothes/ClothingListModel.cs
@@ -6,10 +6,8 @@ namespace AirsoftMatchMaker.Core.Models.Clothes
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public int Quantity { get; set; }
         public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
         public ClothingColor ClothingColor { get; set; }
-        //public int? VendorId { get; set; }
-        //public virtual Vendor? Vendor { get; set; }
     }
 }
