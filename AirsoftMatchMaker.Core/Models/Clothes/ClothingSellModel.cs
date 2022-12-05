f06dd8bf630af72a37dc3cc967c836c2a8e247dc
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:33:56 +0200

    Added sell models for weapons and clothes. A slight alteration of creation model .

---
+++ AirsoftMatchMaker.Core/Models/Clothes/ClothingSellModel.cs
@@ -0,0 +1,20 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftMatchMaker.Core.Models.Clothes
+{
+    public class ClothingSellModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(60)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MinLength(5), MaxLength(200)]
+        public string Description { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public ClothingColor ClothingColor { get; set; }
+        public decimal OldPrice { get; set; }
+    }
+}
---
+++ AirsoftMatchMaker.Core/Models/Weapons/WeaponSellModel.cs
@@ -0,0 +1,20 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftMatchMaker.Core.Models.Weapons
+{
+    public class WeaponSellModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(30)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MinLength(3), MaxLength(200)]
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public WeaponType WeaponType { get; init; }
+        public decimal OldPrice { get; set; }
+    }
+}
