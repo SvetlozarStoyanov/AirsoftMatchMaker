a7a5d1327bd1c28582ccaeabd4256404e262bd5a
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:43:04 +0200

    Added new constants for average ammo expended and changed naming for better readability

--- AirsoftMatchMaker.Core/Common/Constants/WeaponConstants.cs
+++ AirsoftMatchMaker.Core/Common/Constants/WeaponConstants.cs
@@ -2,30 +2,44 @@
 {
     public static class WeaponConstants
     {
-        public const double PistolFireRateMin = 100.0;
-        public const double PistolFireRateMax = 1000.00;
-        public const double SubmachineGunFireRateMin = 400.0;
-        public const double SubmachineGunFireRateMax = 1000.00;
-        public const double ShotgunFireRateMin = 80.00;
-        public const double ShotgunFireRateMax = 400.00;
-        public const double AssaultRifleFireRateMin = 400.0;
-        public const double AssaultRifleFireRateMax = 1500.00;
-        public const double SniperRifleFireRateMin = 20.00;
-        public const double SniperRifleFireRateMax = 150;
-        public const double HeavyFireRateMin = 4.00;
-        public const double HeavyFireRateMax = 6000;
+        public const double PistolMinFeetPerSecond = 100;
+        public const double PistolMaxFeetPerSecond = 350;
+        public const double SubmachineGunMinFeetPerSecond = 150;
+        public const double SubmachineGunMaxFeetPerSecond = 350;
+        public const double ShotgunMinFeetPerSecond = 100;
+        public const double ShotgunMaxFeetPerSecond = 350;
+        public const double AssaultRifleMinFeetPerSecond = 150;
+        public const double AssaultRifleMaxFeetPerSecond = 400;
+        public const double SniperRifleMinFeetPerSecond = 300;
+        public const double SniperRifleMaxFeetPerSecond = 550;
+        public const double HeavyMinFeetPerSecond = 100;
+        public const double HeavyMaxFeetPerSecond = 500;
 
-        public const double PistolFeetPerSecondMin = 100;
-        public const double PistolFeetPerSecondMax = 350;
-        public const double SubmachineGunFeetPerSecondMin = 150;
-        public const double SubmachineGunFeetPerSecondMax = 350;
-        public const double ShotgunFeetPerSecondMin = 100;
-        public const double ShotgunFeetPerSecondMax = 350;
-        public const double AssaultRifleFeetPerSecondMin = 150;
-        public const double AssaultRifleFeetPerSecondMax = 400;
-        public const double SniperRifleFeetPerSecondMin = 300;
-        public const double SniperRifleFeetPerSecondMax = 550;
-        public const double HeavyFeetPerSecondMin = 100;
-        public const double HeavyFeetPerSecondMax = 500;
+        public const double PistolMinFireRate = 100.0;
+        public const double PistolMaxFireRate = 1000.00;
+        public const double SubmachineGunMinFireRate = 400.0;
+        public const double SubmachineGunMaxFireRate = 1000.00;
+        public const double ShotgunMinFireRate = 80.00;
+        public const double ShotgunMaxFireRate = 400.00;
+        public const double AssaultRifleMinFireRate = 400.0;
+        public const double AssaultRifleMaxFireRate = 1500.00;
+        public const double SniperRifleMinFireRate = 20.00;
+        public const double SniperRifleMaxFireRate = 150;
+        public const double HeavyMinFireRate = 4.00;
+        public const double HeavyMaxFireRate = 6000;
+
+        public const double PistolMinAverageAmmoExpended = 30;
+        public const double PistolMaxAverageAmmoExpended = 150;
+        public const double SubmachineGunMinAverageAmmoExpended = 60;
+        public const double SubmachineGunMaxAverageAmmoExpended = 180;
+        public const double ShotgunMinAverageAmmoExpended = 10;
+        public const double ShotgunMaxAverageAmmoExpended = 40;
+        public const double AssaultRifleMinAverageAmmoExpended = 30;
+        public const double AssaultRifleMaxAverageAmmoExpended = 180;
+        public const double SniperRifleMinAverageAmmoExpended = 10;
+        public const double SniperRifleMaxAverageAmmoExpended = 60;
+        public const double HeavyMinAverageAmmoExpended = 5;
+        public const double HeavyMaxAverageAmmoExpended = 6000;
+
     }
 }
