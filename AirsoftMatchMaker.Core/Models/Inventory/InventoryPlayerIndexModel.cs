18220ee5deb6c7d14d44a243e83055facd6225e4
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:49:59 +0200

    Added models which get player or vendor inventory items.

---
+++ AirsoftMatchMaker.Core/Models/Inventory/InventoryPlayerIndexModel.cs
@@ -0,0 +1,18 @@
+﻿using AirsoftMatchMaker.Core.Models.Clothes;
+using AirsoftMatchMaker.Core.Models.Weapons;
+
+namespace AirsoftMatchMaker.Core.Models.Inventory
+{
+    public class InventoryPlayerIndexModel
+    {
+        public InventoryPlayerIndexModel()
+        {
+            Clothes = new List<ClothingListModel>();
+            Weapons = new List<WeaponListModel>();
+        }
+        public int Ammo { get; set; }
+        public ICollection<ClothingListModel> Clothes { get; set; }
+        public ICollection<WeaponListModel> Weapons { get; set; }
+
+    }
+}
---
+++ AirsoftMatchMaker.Core/Models/Inventory/InventoryVendorIndexModel.cs
@@ -0,0 +1,21 @@
+﻿using AirsoftMatchMaker.Core.Models.AmmoBoxes;
+using AirsoftMatchMaker.Core.Models.Clothes;
+using AirsoftMatchMaker.Core.Models.Weapons;
+
+namespace AirsoftMatchMaker.Core.Models.Inventory
+{
+    public class InventoryVendorIndexModel
+    {
+        public InventoryVendorIndexModel()
+        {
+
+            AmmoBoxes = new List<AmmoBoxListModel>();
+            Clothes = new List<ClothingListModel>();
+            Weapons = new List<WeaponListModel>();
+        }
+
+        public ICollection<AmmoBoxListModel> AmmoBoxes { get; set; }
+        public ICollection<ClothingListModel> Clothes { get; set; }
+        public ICollection<WeaponListModel> Weapons { get; set; }
+    }
+}
