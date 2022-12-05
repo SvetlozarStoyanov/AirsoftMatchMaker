6aa995ca7bb0b88e697e683a6ce8a60945cfb9a5
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-05 17:37:34 +0200

    Added inventory controller which shows vendor and player inventory for user

---
+++ AirsoftMatchMaker.Web/Areas/Vendor/Controllers/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using AirsoftMatchMaker.Core.Contracts;
+using AirsoftMatchMaker.Core.Models.Inventory;
+using AirsoftMatchMaker.Web.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirsoftMatchMaker.Web.Areas.Vendor.Controllers
+{
+    [Area("Vendor")]
+    [Authorize(Roles = "Vendor")]
+    public class InventoryController : Controller
+    {
+
+        private readonly IInventoryService inventoryService;
+        public InventoryController(IInventoryService inventoryService)
+        {
+            this.inventoryService = inventoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await inventoryService.GetVendorItemsAsync(User.Id());
+            return View(model);
+        }
+        public async Task<IActionResult> PlayerItems()
+        {
+            var model = await inventoryService.GetPlayerItemsAsync(User.Id());
+            return View(model);
+        }
+    }
+}
