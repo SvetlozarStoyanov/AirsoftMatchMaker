61c142ead12b2c839248cf00d2ffb809b9b7fe8d
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-07 21:43:53 +0200

    Added terrain type

--- AirsoftMatchMaker.Core/Models/Maps/MapMinModel.cs
+++ AirsoftMatchMaker.Core/Models/Maps/MapMinModel.cs
@@ -1,12 +1,9 @@
-﻿using AirsoftMatchMaker.Infrastructure.Data.Enums;
-
-namespace AirsoftMatchMaker.Core.Models.Maps
+﻿namespace AirsoftMatchMaker.Core.Models.Maps
 {
     public class MapMinModel
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string? ImageUrl { get; set; }
-        public TerrainType Terrain { get; set; }
     }
 }
