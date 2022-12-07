06c3c0a4c91579ad14e7025e6542f5306240b09f
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-07 21:36:35 +0200

    Merged Gamelist and GamePartial Models

--- AirsoftMatchMaker.Core/Models/Games/GameListModel.cs
+++ AirsoftMatchMaker.Core/Models/Games/GameListModel.cs
@@ -4,20 +4,29 @@ namespace AirsoftMatchMaker.Core.Models.Games
 {
     public class GameListModel
     {
-        public int Id { get; set; }
 
+        public int Id { get; set; }
         public string Name { get; set; } = null!;
-        public string Date { get; set; } = null!;
-        public decimal EntryFee { get; set; }
+        public DateTime Date { get; set; }
         public GameStatus GameStatus { get; set; }
+
         public int MapId { get; set; }
-        public string MapName { get; set; } = null!;
+        public string MapName { get; set; }
+        public string? MapImageUrl { get; set; }
+        public TerrainType TerrainType { get; set; }
+
+        public string? Result { get; set; }
+        public decimal EntryFee { get; set; }
         public int GameModeId { get; set; }
         public string GameModeName { get; set; } = null!;
-        public int MatchmakerId { get; set; }
-        public string MatchmakerName { get; set; } = null!;
-        public string? Result { get; set; }
+
         public int TeamRedId { get; set; }
+        public string TeamRedName { get; set; } = null!;
+        public int TeamRedOdds { get; set; }
+
         public int TeamBlueId { get; set; }
+        public string TeamBlueName { get; set; } = null!;
+        public int TeamBlueOdds { get; set; }
+
     }
 }
