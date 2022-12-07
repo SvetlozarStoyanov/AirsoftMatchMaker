80cfd8f00063b5489c46166102c7976b150930fb
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-07 21:40:20 +0200

    Added description property

--- AirsoftMatchMaker.Core/Models/GameModes/GameModeListModel.cs
+++ AirsoftMatchMaker.Core/Models/GameModes/GameModeListModel.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
         public int MapsCount { get; set; }
         public int PointsToWin { get; set; }
     }
