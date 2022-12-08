aad4a4697e1de74ccf3bc2166ed05dcd2a4329bb
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-08 23:57:43 +0200

    Injected team service and added create get and post methods

--- AirsoftMatchMaker.Web/Areas/Player/Controllers/TeamsController.cs
+++ AirsoftMatchMaker.Web/Areas/Player/Controllers/TeamsController.cs
@@ -1,4 +1,5 @@
 ﻿using AirsoftMatchMaker.Core.Contracts;
+using AirsoftMatchMaker.Core.Models.Teams;
 using AirsoftMatchMaker.Web.Extensions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,13 +11,16 @@ namespace AirsoftMatchMaker.Web.Areas.Player.Controllers
     public class TeamsController : Controller
     {
         private readonly ITeamService teamService;
-        public TeamsController(ITeamService teamService)
+        private readonly ITeamRequestService teamRequestService;
+        public TeamsController(ITeamService teamService, ITeamRequestService teamRequestService)
         {
             this.teamService = teamService;
+            this.teamRequestService = teamRequestService;
         }
 
         public async Task<IActionResult> Index()
         {
+            ViewBag.UserHasTeam = await teamService.DoesUserHaveTeamAsync(User.Id());
             var model = await teamService.GetAllTeamsAsync();
             return View(model);
         }
@@ -34,13 +38,48 @@ namespace AirsoftMatchMaker.Web.Areas.Player.Controllers
 
         public async Task<IActionResult> MyTeam()
         {
-            if (!(await teamService.DoesPlayerHaveTeam(User.Id())))
+            if (!(await teamService.DoesUserHaveTeamAsync(User.Id())))
             {
                 TempData["error"] = $"You don't have a team!";
                 return RedirectToAction(nameof(Index));
             }
             var model = await teamService.GetPlayersTeamAsync(User.Id());
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            if (await teamRequestService.DoesTheUserHaveAcceptedOrPendingTeamRequestsAsync(User.Id()))
+            {
+                TempData["error"] = "You can only create a team if you cancel your requests to join another!";
+                return RedirectToAction("Mine", "TeamRequests");
+            }
+            if (await teamService.DoesUserHaveTeamAsync(User.Id()))
+            {
+                TempData["error"] = "Cannot create team when part of other!";
+                return RedirectToAction(nameof(Index));
+
+            }
+            var model = new TeamCreateModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TeamCreateModel model)
+        {
+            if (await teamService.DoesTeamWithSameNameExistAsync(model.Name))
+            {
+                TempData["error"] = "Team with that name already exists!";
+                return View(model);
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            await teamService.CreateTeamAsync(User.Id(), model);
+            return RedirectToAction(nameof(MyTeam));
+
         }
     }
 }
