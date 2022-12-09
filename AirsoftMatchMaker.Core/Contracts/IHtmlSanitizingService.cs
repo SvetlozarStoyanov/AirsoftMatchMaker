d7b7b2c83609a7f806bc351cdc7d02ba6f4995ab
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-09 20:52:16 +0200

    Added service which utilized HtmlSanitizer by Ganss.It sanitizes given string property or sanitizes all string properties in given object by utilizing a generic method and reflection

---
+++ AirsoftMatchMaker.Core/Contracts/IHtmlSanitizingService.cs
@@ -0,0 +1,9 @@
+﻿namespace AirsoftMatchMaker.Core.Contracts
+{
+    public interface IHtmlSanitizingService
+    {
+        T SanitizeObject<T>(T obj);
+
+        string SanitizeStringProperty(string stringProperty);
+    }
+}
