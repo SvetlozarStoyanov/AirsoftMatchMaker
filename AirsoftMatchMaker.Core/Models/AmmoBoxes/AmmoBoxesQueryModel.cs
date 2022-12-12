bc549b7d5e278ed374688911dd0ad4222716ac8a
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-12 15:36:37 +0200

    Added AmmoBoxesQueryModel  for ammo box sorting

---
+++ AirsoftMatchMaker.Core/Models/AmmoBoxes/AmmoBoxesQueryModel.cs
@@ -0,0 +1,27 @@
+﻿using AirsoftMatchMaker.Core.Models.Clothes;
+using AirsoftMatchMaker.Core.Models.Enums;
+using AirsoftMatchMaker.Infrastructure.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftMatchMaker.Core.Models.AmmoBoxes
+{
+    public class AmmoBoxesQueryModel
+    {
+        public AmmoBoxesQueryModel()
+        {
+            AmmoBoxes = new HashSet<AmmoBoxListModel>();
+            SortingOptions = new HashSet<AmmoBoxSorting>();
+        }
+        public string? SearchTerm { get; set; }
+        public AmmoBoxSorting Sorting { get; set; } = AmmoBoxSorting.AmmoAmount;
+        public int AmmoBoxesPerPage { get; set; } = 6;
+        public int CurrentPage { get; set; } = 1;
+        public int AmmoBoxesCount { get; set; }
+        public ICollection<AmmoBoxSorting> SortingOptions { get; set; }
+        public IEnumerable<AmmoBoxListModel> AmmoBoxes { get; set; }
+    }
+}
