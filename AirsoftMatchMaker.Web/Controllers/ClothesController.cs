beb63df520df3944b442e09a261ea1f47c864baf
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-12 15:53:09 +0200

    Added clothing sorting to clothes index view

--- AirsoftMatchMaker.Web/Controllers/ClothesController.cs
+++ AirsoftMatchMaker.Web/Controllers/ClothesController.cs
@@ -1,4 +1,5 @@
 ﻿using AirsoftMatchMaker.Core.Contracts;
+using AirsoftMatchMaker.Core.Models.Clothes;
 using AirsoftMatchMaker.Web.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +8,28 @@ namespace AirsoftMatchMaker.Web.Controllers
     public class ClothesController : Controller
     {
         private readonly IClothingService clothingService;
-        public ClothesController(IClothingService clothingService)
+        private IHtmlSanitizingService htmlSanitizingService;
+        public ClothesController(IClothingService clothingService, IHtmlSanitizingService htmlSanitizingService)
         {
             this.clothingService = clothingService;
+            this.htmlSanitizingService = htmlSanitizingService;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery]ClothesQueryModel model)
         {
-            var model = await clothingService.GetAllClothesAsync();
+            model.SearchTerm = htmlSanitizingService.SanitizeStringProperty(model.SearchTerm);
+            var queryResult = await clothingService.GetAllClothesAsync
+                (
+                    model.ClothingColor,
+                    model.Sorting,
+                    model.SearchTerm,
+                    model.ClothesPerPage,
+                    model.CurrentPage
+                );
+            model.Clothes = queryResult.Clothes;
+            model.Colors = queryResult.Colors;
+            model.SortingOptions = queryResult.SortingOptions;
+            model.ClothesCount = queryResult.ClothesCount;
             return View(model);
         }
 
