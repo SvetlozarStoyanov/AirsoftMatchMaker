edcd7493eebad06d76fac8a75ab9ed7fd8c9a6f7
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 20:16:40 +0200

    Added new database table responsible for bet money management for a single game and filled its config file accordingly.

---
+++ AirsoftMatchMaker.Infrastructure/Data/Configuration/GameBetCreditsContainerConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AirsoftMatchMaker.Infrastructure.Data.Configuration
+{
+    public class GameBetCreditsContainerConfiguration : IEntityTypeConfiguration<GameBetCreditsContainer>
+    {
+        public void Configure(EntityTypeBuilder<GameBetCreditsContainer> builder)
+        {
+            builder.HasData(CreateGameBetCreditsContainers());
+        }
+
+        private List<GameBetCreditsContainer> CreateGameBetCreditsContainers()
+        {
+            var gameBetCreditsContainers = new List<GameBetCreditsContainer>()
+            {
+                new GameBetCreditsContainer()
+                {
+                    Id = 1,
+                    GameId = 1,
+                    TeamRedCreditsBet = 50,
+                    TeamBlueCreditsBet = 40
+                },
+                new GameBetCreditsContainer()
+                {
+                    Id = 2,
+                    GameId = 2,
+                    TeamRedCreditsBet = 0,
+                    TeamBlueCreditsBet = 0
+                },
+                new GameBetCreditsContainer()
+                {
+                    Id = 3,
+                    GameId = 3,
+                    TeamRedCreditsBet = 0,
+                    TeamBlueCreditsBet = 0
+                }
+            };
+            return gameBetCreditsContainers;
+        }
+    }
+}
---
+++ AirsoftMatchMaker.Infrastructure/Data/Entities/GameBetCreditsContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirsoftMatchMaker.Infrastructure.Data.Entities
+{
+    public class GameBetCreditsContainer
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey(nameof(Entities.Game.Id))]
+        public int GameId { get; set; }
+        public Game Game { get; set; } = null!;
+        public decimal TeamRedCreditsBet { get; set; }
+        public decimal TeamBlueCreditsBet { get; set; }
+        public bool BetsArePaidOut { get; set; } = false;
+    }
+}
