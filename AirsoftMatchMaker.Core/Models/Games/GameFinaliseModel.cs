f7d57637b918da5f51766cfe6b2e1008ba258dc5
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 20:09:47 +0200

    Added new model for finalising games. It contains max points and requires input for team red and team blue points

---
+++ AirsoftMatchMaker.Core/Models/Games/GameFinaliseModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftMatchMaker.Core.Models.Games
+{
+    public class GameFinaliseModel
+    {
+        public int Id { get; set; }
+        public int GameModeMaxPoints { get; set; }
+        [Required(ErrorMessage = "Required!")]
+        [Range(0, 100, ErrorMessage = "Points must be within the given limit")]
+        public int TeamRedPoints { get; set; }
+        [Required(ErrorMessage = "Required")]
+        [Range(0, 100, ErrorMessage = "Points must be within the given limit")]
+        [NotEqual(nameof(TeamRedPoints), ErrorMessage = "Points cannot be equal!")]
+        public int TeamBluePoints { get; set; }
+    }
+}
