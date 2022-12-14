31f7724ef6b2d4449fef581021f3c15f9863b46c
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 19:57:14 +0200

    Added background bet service which pays out bets of finalised games

---
+++ AirsoftMatchMaker.Core/Contracts/IBackgroundBetService.cs
@@ -0,0 +1,20 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Entities;
+
+namespace AirsoftMatchMaker.Core.Contracts
+{
+    public interface IBackgroundBetService
+    {
+        /// <summary>
+        /// Returns the ids of <see cref="Game"/> who have been finished but whose <see cref="Game.Bets"/> have not
+        /// been paid out yet
+        /// </summary>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
+        Task<IEnumerable<int>> GetAllFinalisedGameIdsWithUnpaidBetsAsync();
+
+        /// <summary>
+        /// Pays out the profit to the bettors who correctly predicted the result of the <see cref="Game game"/> with given id.
+        /// </summary>
+        /// <returns></returns>
+        Task PayoutBetsByGameIdAsync(int gameId);
+    }
+}
