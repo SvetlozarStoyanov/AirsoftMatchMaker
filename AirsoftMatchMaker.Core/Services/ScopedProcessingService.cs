e7ef05f72a1b714e655c71b5ea88cfd475f5e1ce
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 20:15:20 +0200

    Now uses new background bet and game services instead of game simulation service

--- AirsoftMatchMaker.Core/Services/ScopedProcessingService.cs
+++ AirsoftMatchMaker.Core/Services/ScopedProcessingService.cs
@@ -6,11 +6,14 @@ namespace AirsoftMatchMaker.Core.Services
     public class ScopedProcessingService : IScopedProcessingService
     {
         private PeriodicTimer timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
-        private readonly IGameSimulationService gameSimulationService;
+        //private readonly IGameSimulationService gameSimulationService;
         private readonly IBackgroundTeamService backgroundTeamService;
-        public ScopedProcessingService(IGameSimulationService gameSimulationService, IBackgroundTeamService backgroundTeamService)
+        private readonly IBackgroundGameService backgroundGameService;
+        private readonly IBackgroundBetService backgroundBetService;
+        public ScopedProcessingService(IBackgroundGameService backgroundGameService, IBackgroundBetService backgroundBetService, IBackgroundTeamService backgroundTeamService)
         {
-            this.gameSimulationService = gameSimulationService;
+            this.backgroundGameService = backgroundGameService;
+            this.backgroundBetService = backgroundBetService;
             this.backgroundTeamService = backgroundTeamService;
         }
 
@@ -18,12 +21,17 @@ namespace AirsoftMatchMaker.Core.Services
         {
             do
             {
-                var gameIds = await gameSimulationService.FindGamesToSimulateAsync(DateTime.Now);
-                foreach (var gameId in gameIds)
+                var gamesToUpdateBettingOddsIds = await backgroundGameService.GetGameIdsOfGamesWithNotUpDoDateOddsAsync();
+                foreach (var gameId in gamesToUpdateBettingOddsIds)
                 {
-                    await gameSimulationService.SimulateGameAsync(gameId);
-                    await gameSimulationService.PayoutBetsByGameIdAsync(gameId);
-                    Console.WriteLine($"Game with id {gameId} has been simulated and it's bets have been paid out!");
+                    await backgroundGameService.CalculateBettingOddsAsync(gameId);
+                    Console.WriteLine($"Game with id {gameId} odds have been calculated!");
+                }
+                await backgroundGameService.MarkGamesAsFinishedAsync(DateTime.Now);
+                var gamesToHaveTheirBetsPayedOutIds = await backgroundBetService.GetAllFinalisedGameIdsWithUnpaidBetsAsync();
+                foreach (var gameId in gamesToHaveTheirBetsPayedOutIds)
+                {
+                    await backgroundBetService.PayoutBetsByGameIdAsync(gameId);
                 }
                 var teamRequestIds = await backgroundTeamService.GetPlayersToAssignOrRemoveFromTeamsTeamRequestIdsAsync();
                 if (teamRequestIds.Any())
