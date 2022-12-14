2c4bec60b73f0a1930cf1abfa40d5f55dd2dfc0a
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 20:06:56 +0200

    Added player class service which assigns player's to classes. It also contains validation methods for player class existing and player already being in a given class.

---
+++ AirsoftMatchMaker.Core/Contracts/IPlayerClassService.cs
@@ -0,0 +1,49 @@
+﻿using AirsoftMatchMaker.Core.Models.PlayerClasses;
+using AirsoftMatchMaker.Infrastructure.Data.Entities;
+
+namespace AirsoftMatchMaker.Core.Contracts
+{
+    public interface IPlayerClassService
+    {
+        /// <summary>
+        /// Returns true if <see cref="PlayerClass"/> exists, otherwise returns false.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns><see cref="bool"/></returns>
+        Task<bool> DoesPlayerClassExistAsync(int id);
+
+        /// <summary>
+        /// Returns true if <see cref="Player"/> with given <paramref name="userId"/> is already assigned
+        /// to  <see cref="PlayerClass"/> with given <paramref name="playerClassId"/>, otherwise returns false.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="playerClassId"></param>
+        /// <returns></returns>
+        Task<bool> IsPlayerAlreadyInPlayerClass(string userId,int playerClassId);
+
+        /// <summary>
+        /// Returns all <see cref="PlayerClass"/> from the database
+        /// </summary>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
+        Task<IEnumerable<PlayerClassListModel>> GetAllPlayerClassesAsync();
+
+        /// <summary>
+        /// Returns <see cref="PlayerClass.Id"/> of <see cref="PlayerClass"/> of <see cref="Player"/>
+        /// with given <paramref name="userId"/>
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns><see cref="int"/></returns>
+        Task<int> GetPlayersPlayerClassIdByUserIdAsync(string userId);
+
+        /// <summary>
+        /// Assigns <see cref="Player"/> to <see cref="PlayerClass"/> with given <paramref name="playerClassId"/>
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="playerClassId"></param>
+        /// <returns></returns>
+        Task ChangePlayerClassAsync(string userId, int playerClassId);
+
+
+        //Task RemovePlayerFromPlayerClassAsync(string userId, int playerClassId);
+    }
+}
---
+++ AirsoftMatchMaker.Core/Services/PlayerClassService.cs
@@ -0,0 +1,73 @@
+﻿using AirsoftMatchMaker.Core.Contracts;
+using AirsoftMatchMaker.Core.Models.PlayerClasses;
+using AirsoftMatchMaker.Infrastructure.Data.Common.Repository;
+using AirsoftMatchMaker.Infrastructure.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirsoftMatchMaker.Core.Services
+{
+    public class PlayerClassService : IPlayerClassService
+    {
+        private readonly IRepository repository;
+
+        public PlayerClassService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<bool> DoesPlayerClassExistAsync(int id)
+        {
+            return await repository.GetByIdAsync<PlayerClass>(id) != null;
+        }
+
+        public async Task<bool> IsPlayerAlreadyInPlayerClass(string userId, int playerClassId)
+        {
+            var player = await repository.AllReadOnly<Player>()
+                .FirstOrDefaultAsync(p => p.UserId == userId);
+            if (player.PlayerClassId == playerClassId)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<IEnumerable<PlayerClassListModel>> GetAllPlayerClassesAsync()
+        {
+            var playerClasses = await repository.AllReadOnly<PlayerClass>()
+                .Where(p => p.Id > 1)
+                .Select(p => new PlayerClassListModel()
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description
+                })
+                .ToListAsync();
+            return playerClasses;
+        }
+
+        public async Task<int> GetPlayersPlayerClassIdByUserIdAsync(string userId)
+        {
+            var playerClassId = await repository.AllReadOnly<Player>()
+                .Where(p => p.UserId == userId)
+                .Select(p => p.PlayerClassId)
+                .FirstOrDefaultAsync();
+            return playerClassId;
+        }
+
+        public async Task ChangePlayerClassAsync(string userId, int playerClassId)
+        {
+            var player = await repository.All<Player>()
+                .FirstOrDefaultAsync(p => p.UserId == userId);
+            player.PlayerClassId = playerClassId;
+            await repository.SaveChangesAsync();
+        }
+
+        //public async Task RemovePlayerFromPlayerClassAsync(string userId, int playerClassId)
+        //{
+        //    var player = await repository.AllReadOnly<Player>()
+        //        .FirstOrDefaultAsync(p => p.UserId == userId);
+        //    player.PlayerClassId = 1;
+        //    await repository.SaveChangesAsync();
+        //}
+    }
+}
