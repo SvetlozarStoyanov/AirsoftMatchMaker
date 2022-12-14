82750cfed1362b0f22b0cc0c8da8bbd99981a792
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 20:10:51 +0200

    Moved team properties to TeamMinModels

--- AirsoftMatchMaker.Core/Models/Games/GameViewModel.cs
+++ AirsoftMatchMaker.Core/Models/Games/GameViewModel.cs
@@ -1,4 +1,6 @@
-﻿using AirsoftMatchMaker.Core.Models.Players;
+﻿using AirsoftMatchMaker.Core.Models.Maps;
+using AirsoftMatchMaker.Core.Models.Players;
+using AirsoftMatchMaker.Core.Models.Teams;
 using AirsoftMatchMaker.Infrastructure.Data.Enums;
 
 namespace AirsoftMatchMaker.Core.Models.Games
@@ -7,32 +9,21 @@ namespace AirsoftMatchMaker.Core.Models.Games
     {
 
         public int Id { get; set; }
-
         public string Name { get; set; } = null!;
         public string Date { get; set; }
         public decimal EntryFee { get; set; }
         public GameStatus GameStatus { get; set; }
-        public int MapId { get; set; }
-        public string MapName { get; set; } = null!;
+        public string? Result { get; set; }
+
+        public MapMinModel Map { get; set; }
         public int GameModeId { get; set; }
         public string GameModeName { get; set; } = null!;
         public int MatchmakerId { get; set; }
         public string MatchmakerName { get; set; }
-        public string? Result { get; set; }
 
-        public int TeamRedId { get; set; }
-        public string TeamRedName { get; set; } = null!;
-        public ICollection<PlayerMinModel> TeamRedPlayers { get; set; }
-        public int TeamRedPoints { get; set; }
-        public int TeamRedBets { get; set; }
-        public int TeamRedOdds { get; set; }
+        public TeamMinModel TeamRed { get; set; }
 
-        public int TeamBlueId { get; set; }
-        public string TeamBlueName { get; set; } = null!;
-        public ICollection<PlayerMinModel> TeamBluePlayers { get; set; }
-        public int TeamBluePoints { get; set; }
-        public int TeamBlueBets { get; set; }
-        public int TeamBlueOdds { get; set; }
+        public TeamMinModel TeamBlue { get; set; }
 
     }
 }
