3c82127b6967ad8a171f613a2f3e43aa31c83145
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-14 19:59:42 +0200

    Removed unnecessary commented lines and added documentation

--- AirsoftMatchMaker.Core/Contracts/IBackgroundTeamService.cs
+++ AirsoftMatchMaker.Core/Contracts/IBackgroundTeamService.cs
@@ -2,10 +2,17 @@
 {
     public interface IBackgroundTeamService
     {
-
+        /// <summary>
+        /// Gets ids of team requests which can be fulfilled.
+        /// </summary>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
         Task<IEnumerable<int>> GetPlayersToAssignOrRemoveFromTeamsTeamRequestIdsAsync();
 
-
+        /// <summary>
+        /// Fulfills team requests with given ids
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
         Task AssignOrRemovePlayerFromTeamByTeamRequestIdsAsync(IEnumerable<int> ids);
     }
 }
--- AirsoftMatchMaker.Core/Services/BackgroundTeamService.cs
+++ AirsoftMatchMaker.Core/Services/BackgroundTeamService.cs
@@ -52,7 +52,6 @@ namespace AirsoftMatchMaker.Core.Services
                         break;
                 }
             }
-            //await repository.SaveChangesAsync();
             repository.DeleteRange<TeamRequest>(teamRequests);
             await repository.SaveChangesAsync();
 
