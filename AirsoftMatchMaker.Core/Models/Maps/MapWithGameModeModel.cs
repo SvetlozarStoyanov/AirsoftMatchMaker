ad97c6a221aa54cb92bc4a2a3f557dce312f610a
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-15 16:41:03 +0200

    New Model which contains MapMinModel properites and gamemode id and name

---
+++ AirsoftMatchMaker.Core/Models/Maps/MapWithGameModeModel.cs
@@ -0,0 +1,14 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Enums;
+
+namespace AirsoftMatchMaker.Core.Models.Maps
+{
+    public class MapWithGameModeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public TerrainType Terrain { get; set; }
+        public int GameModeId { get; set; }
+        public string GameModeName { get; set; } = null!;
+    }
+}
