cd7a0601b51d1a33c23d4bf80a3194589058b4e3
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-17 19:55:21 +0200

    Added new sorting enum which is like GameStatus but adds finalised value.

---
+++ AirsoftMatchMaker.Core/Models/Enums/MatchmakerGameStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftMatchMaker.Core.Models.Enums
+{
+    public enum MatchmakerGameStatus
+    {
+        Upcoming,
+        Finished,
+        Finalized
+    }
+}
