633a55bf750ada7dfc306d6ad40689f80fc02729
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-17 19:54:25 +0200

    Added create models for map and game mode entities

---
+++ AirsoftMatchMaker.Core/Models/GameModes/GameModeCreateModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftMatchMaker.Core.Models.GameModes
+{
+    public class GameModeCreateModel
+    {
+        [Required]
+        [MinLength(5), MaxLength(30)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MinLength(5), MaxLength(200)]
+        public string Description { get; set; } = null!;
+        [Range(1, 100)]
+        public int PointsToWin { get; set; }
+
+    }
+}
---
+++ AirsoftMatchMaker.Core/Models/Maps/MapCreateModel.cs
@@ -0,0 +1,34 @@
+﻿using AirsoftMatchMaker.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftMatchMaker.Core.Models.Maps
+{
+    public class MapCreateModel
+    {
+        public MapCreateModel()
+        {
+            GameModeIds = new List<int>();
+            GameModeNames = new List<string>();
+            Mapsizes = new List<Mapsize>();
+            TerrainTypes = new List<TerrainType>();
+            AverageEngagementDistances = new List<AverageEngagementDistance>();
+        }
+
+        [Required]
+        [MinLength(3), MaxLength(60)]
+        public string Name { get; set; } = null!;
+        [Required]
+        [MinLength(5), MaxLength(200)]
+        public string Description { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+        public Mapsize Mapsize { get; set; }
+        public TerrainType TerrainType { get; set; }
+        public AverageEngagementDistance AverageEngagementDistance { get; set; }
+        public int GameModeId { get; set; }
+        public ICollection<string> GameModeNames { get; set; }
+        public ICollection<int> GameModeIds { get; set; }
+        public ICollection<Mapsize> Mapsizes { get; set; }
+        public ICollection<TerrainType> TerrainTypes { get; set; }
+        public ICollection<AverageEngagementDistance> AverageEngagementDistances { get; set; }
+    }
+}
