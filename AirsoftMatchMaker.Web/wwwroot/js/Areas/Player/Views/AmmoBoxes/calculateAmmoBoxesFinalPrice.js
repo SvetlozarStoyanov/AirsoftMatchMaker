c4839d1d27c3507ea4b8407766232a553af684ce
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-19 19:40:42 +0200

    Added js files to validate if user can buy selected amount of ammoBoxes. The scripts are identical ,one works for view,  the other for partial view in modal.

---
+++ AirsoftMatchMaker.Web/wwwroot/js/Areas/Player/Views/AmmoBoxes/calculateAmmoBoxesFinalPrice.js
@@ -0,0 +1,49 @@
+﻿const ammoAmountInitial = document.getElementById('ammoAmountInitial');
+const ammoAmountFinal = document.getElementById('ammoAmountFinal');
+const pricePerUnit = document.getElementById('pricePerUnit');
+const quantityToBuy = document.getElementById('quantityToBuy');
+const finalPriceForSubmit = document.getElementById('finalPriceForSubmit');
+const finalPriceForUi = document.getElementById('finalPriceForUi');
+const userCredits = parseFloat( document.getElementById('userCredits').value);
+const buyBtn = document.getElementById('buyBtn');
+
+console.log(ammoAmountInitial.value)
+calculatePriceAndUpdateAmmoCount();
+
+function updatePricePerUnit() {
+    var NameValue = pricePerUnit.value;
+    // use it
+    console.log(NameValue);
+    calculatePriceAndUpdateAmmoCount();
+}
+pricePerUnit.onchange = updatePricePerUnit;
+pricePerUnit.onblur = updatePricePerUnit;
+
+function updateQuantityToBuy() {
+    var NameValue = quantityToBuy.value;
+    // use it
+    console.log(NameValue);
+    calculatePriceAndUpdateAmmoCount();
+}
+
+quantityToBuy.onchange = updateQuantityToBuy;
+quantityToBuy.onblur = updateQuantityToBuy;
+
+function updateBuyBtnStatus() {
+    if (parseFloat(finalPriceForSubmit.value) > userCredits) {
+        buyBtn.disabled = true;
+    } else {
+        buyBtn.disabled = false;
+    }
+}
+
+
+function calculatePriceAndUpdateAmmoCount() {
+    console.log(pricePerUnit.value);
+    console.log(quantityToBuy.value);
+    finalPriceForSubmit.value = parseInt(quantityToBuy.value) * parseFloat(pricePerUnit.value);
+    finalPriceForUi.value = finalPriceForSubmit.value;
+    ammoAmountFinal.value = parseInt(quantityToBuy.value) * parseInt(ammoAmountInitial.value);
+    updateBuyBtnStatus();
+    console.log(`Final  price: ${finalPriceForSubmit.value} credits. Ammo count: ${ammoAmountFinal.value}`);
+}
---
+++ AirsoftMatchMaker.Web/wwwroot/js/Areas/Player/Views/AmmoBoxes/calculateAmmoBoxesFinalPriceModal.js
@@ -0,0 +1,49 @@
+﻿const ammoAmountInitial = document.getElementById('ammoAmountInitial');
+const ammoAmountFinal = document.getElementById('ammoAmountFinal');
+const pricePerUnit = document.getElementById('pricePerUnit');
+const quantityToBuy = document.getElementById('quantityToBuy');
+const finalPriceForSubmit = document.getElementById('finalPriceForSubmit');
+const finalPriceForUi = document.getElementById('finalPriceForUi');
+const userCreditsModal = parseFloat( document.getElementById('userCreditsModal').value);
+const buyBtnModal = document.getElementById('buyBtnModal');
+
+console.log(ammoAmountInitial.value)
+calculatePriceAndUpdateAmmoCount();
+
+function updatePricePerUnit() {
+    var NameValue = pricePerUnit.value;
+    // use it
+    console.log(NameValue);
+    calculatePriceAndUpdateAmmoCount();
+}
+pricePerUnit.onchange = updatePricePerUnit;
+pricePerUnit.onblur = updatePricePerUnit;
+
+function updateQuantityToBuy() {
+    var NameValue = quantityToBuy.value;
+    // use it
+    console.log(NameValue);
+    calculatePriceAndUpdateAmmoCount();
+}
+
+quantityToBuy.onchange = updateQuantityToBuy;
+quantityToBuy.onblur = updateQuantityToBuy;
+
+function updateBuyBtnModalStatus() {
+    if (parseFloat(finalPriceForSubmit.value) > userCreditsModal) {
+        buyBtnModal.disabled = true;
+    } else {
+        buyBtnModal.disabled = false;
+    }
+}
+
+
+function calculatePriceAndUpdateAmmoCount() {
+    console.log(pricePerUnit.value);
+    console.log(quantityToBuy.value);
+    finalPriceForSubmit.value = parseInt(quantityToBuy.value) * parseFloat(pricePerUnit.value);
+    finalPriceForUi.value = finalPriceForSubmit.value;
+    ammoAmountFinal.value = parseInt(quantityToBuy.value) * parseInt(ammoAmountInitial.value);
+    updateBuyBtnModalStatus();
+    console.log(`Final  price: ${finalPriceForSubmit.value} credits. Ammo count: ${ammoAmountFinal.value}`);
+}
