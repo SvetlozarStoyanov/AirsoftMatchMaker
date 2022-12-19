2c20c9e97e5e0c7fafb7ef242fb5951cb8cbf386
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-19 19:30:50 +0200

    Added new model for map sorting

---
+++ AirsoftMatchMaker.Core/Models/Maps/MapsQueryModel.cs
@@ -0,0 +1,28 @@
+﻿using AirsoftMatchMaker.Core.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftMatchMaker.Core.Models.Maps
+{
+    public class MapsQueryModel
+    {
+        public MapsQueryModel()
+        {
+            GameModeNames = new List<string>();
+            Maps = new HashSet<MapListModel>();
+            SortingOptions = new List<MapSorting>();
+        }
+        public int MapsPerPage { get; set; } = 6;
+        public int CurrentPage { get; set; } = 1;
+        public string? SearchTerm { get; set; }
+        public string? GameModeName { get; set; }
+        public MapSorting Sorting { get; set; }
+        public int MapsCount { get; set; }
+        public ICollection<string> GameModeNames { get; set; }
+        public ICollection<MapSorting> SortingOptions { get; set; }
+        public IEnumerable<MapListModel> Maps { get; set; }
+    }
+}
