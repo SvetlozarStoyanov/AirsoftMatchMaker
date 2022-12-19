fe1b155e91946dc9e6da18bc7ae47e99dcb4d950
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-19 19:39:14 +0200

    js files which validate if user has enough credits to buy clothing/weapon

---
+++ AirsoftMatchMaker.Web/wwwroot/js/Areas/Player/Views/Weapons/canBuyWeapon.js
@@ -0,0 +1,10 @@
+﻿const userCredits = parseFloat(document.getElementById('userCredits').value);
+const buyBtn = document.getElementById('buyBtn');
+const price = parseFloat(document.getElementById('price').value);
+
+if (price > userCredits) {
+    buyBtn.disabled = true;
+} else {
+    buyBtn.disabled = false;
+}
+console.log(userCredits)
