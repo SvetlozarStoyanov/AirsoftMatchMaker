88037f465f6b6339bf8d42f28acbcc650baedb23
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-19 19:30:59 +0200

    Added new enum for map sorting

---
+++ AirsoftMatchMaker.Core/Models/Enums/MapSorting.cs
@@ -0,0 +1,25 @@
+﻿namespace AirsoftMatchMaker.Core.Models.Enums
+{
+    /// <summary>
+    /// Enum used for sorting maps 
+    /// </summary>
+    public enum MapSorting
+    {
+        /// <summary>
+        /// Orders maps by Id descending
+        /// </summary>
+        Newest,
+        /// <summary>
+        /// Orders maps by Id ascending
+        /// </summary>
+        Oldest,
+        /// <summary>
+        /// Orders maps by number of games played ascending
+        /// </summary>
+        GamesPlayedAscending,
+        /// <summary>
+        /// Orders maps by number of games played descending
+        /// </summary>
+        GamesPlayedDescending,
+    }
+}
