16e713bd8e263e98106e168cbed03a9c1eee4342
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-20 18:02:06 +0200

    Added class for testing htmlSanitizingService

---
+++ AirsoftMatchMaker.Tests/HelperClasses/TestModel.cs
@@ -0,0 +1,19 @@
+﻿using AirsoftMatchMaker.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftMatchMaker.Tests.HelperClasses
+{
+    /// <summary>
+    /// Model used for testing <see cref="HtmlSanitizingService"/>
+    /// </summary>
+    public class TestModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
