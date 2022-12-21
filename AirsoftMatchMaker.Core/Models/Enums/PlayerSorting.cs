d8e781a2bc5b572aeccb706c05a3b981371f7fe4
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-21 17:38:24 +0200

    Added models/ enums for player sorting

---
+++ AirsoftMatchMaker.Core/Models/Enums/PlayerSorting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftMatchMaker.Core.Models.Enums
+{
+    /// <summary>
+    /// Enum used for sorting players 
+    /// </summary>
+    public enum PlayerSorting
+    {
+        /// <summary>
+        /// Orders items by Id descending
+        /// </summary>
+        Newest,
+        /// <summary>
+        /// Orders items by Id ascending
+        /// </summary>
+        Oldest,
+        /// <summary>
+        /// Orders items by skill level ascending
+        /// </summary>
+        SkillLevelAscending,
+        /// <summary>
+        /// Orders items by skill level descending
+        /// </summary>
+        SkillLevelDescending,
+    }
+}
---
+++ AirsoftMatchMaker.Core/Models/Players/PlayersQueryModel.cs
@@ -0,0 +1,28 @@
+﻿using AirsoftMatchMaker.Core.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirsoftMatchMaker.Core.Models.Players
+{
+        /// <summary>
+        /// Model used for sorting/filtering players
+        /// </summary>
+    public class PlayersQueryModel
+    {
+        public PlayersQueryModel()
+        {
+            SortingOptions = new List<PlayerSorting>();
+            Players = new HashSet<PlayerListModel>();
+        }
+        public int PlayersPerPage { get; set; } = 12;
+        public string? SearchTerm { get; set; }
+        public int CurrentPage { get; set; } = 1;
+        public PlayerSorting Sorting { get; set; }
+        public int PlayersCount { get; set; }
+        public ICollection<PlayerSorting> SortingOptions { get; set; }
+        public IEnumerable<PlayerListModel> Players { get; set; }
+    }
+}
