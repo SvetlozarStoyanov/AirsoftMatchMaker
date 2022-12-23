7040ed4f8b93587236e7409078f37fff7d7f67b6
Svetlozar Stoyanov <svetlio214@gmail.com>
2022-12-23 10:11:55 +0200

    Now uses default area home/index action/view

--- AirsoftMatchMaker.Web/Areas/Vendor/Controllers/HomeController.cs
+++ AirsoftMatchMaker.Web/Areas/Vendor/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using AirsoftMatchMaker.Web.Models;
+﻿using AirsoftMatchMaker.Core.Contracts;
+using AirsoftMatchMaker.Core.Services;
+using AirsoftMatchMaker.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -10,21 +12,22 @@ namespace AirsoftMatchMaker.Web.Areas.Vendor.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IGameService gameService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IGameService gameService)
         {
             _logger = logger;
+            this.gameService = gameService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var model = await gameService.GetUpcomingGamesByDateAsync();
+            return View(model);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
